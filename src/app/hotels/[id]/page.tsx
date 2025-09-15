a981a2eb17007e6c8a1b768421f6b71481227aba
Mastereloading <guilherme-lamon@hotmail.com>
2025-09-15 15:04:58 -0300

    feature: Finished hotel details screen

--- src/app/hotels/[id]/page.tsx
+++ src/app/hotels/[id]/page.tsx
@@ -40,24 +40,36 @@ export default function HotelPage() {
   if (!hotel) return <p style={{ padding: "32px" }}>Hotel não encontrado.</p>
 
   return (
-    <div style={{ padding: "32px", display: "flex", flexDirection: "column", alignItems: "center", minHeight: "100vh", backgroundColor: "var(--hotel-light-gray)" }}>
+    <div style={{ padding: "32px", display: "flex", flexDirection: "column", alignItems: "center", minHeight: "80vh", backgroundColor: "var(--hotel-light-gray)" }}>
       <SearchBar initialDestination="" />
       <div style={{ width: "100%", maxWidth: "1200px", backgroundColor: "var(--hotel-white)", borderRadius: "24px", boxShadow: "0 10px 25px rgba(0,0,0,0.15)", display: "flex", flexDirection: "row", gap: "24px", marginTop: "32px", padding: "24px", flexWrap: "wrap" }}>
-        <div style={{  boxShadow: "0 1px 25px rgba(0,0,0,0.15)", flexShrink: 0, width: "447px", height: "312px", overflow: "hidden", borderRadius: "24px" }}>
+        <div style={{ boxShadow: "0 1px 25px rgba(0,0,0,0.15)", flexShrink: 0, width: "447px", height: "312px", overflow: "hidden", borderRadius: "24px" }}>
           <img src={hotel.hotel.image} alt={hotel.hotel.name} style={{ width: "100%", height: "100%", objectFit: "cover" }} />
         </div>
         <div style={{ flex: 1, display: "flex", flexDirection: "column" }}>
-          <h1 style={{ fontSize: "28px", fontWeight: 600, marginBottom: "8px", color: "var(--hotel-text)" }}>{hotel.hotel.name}</h1>
-          <p style={{ fontWeight: 500, marginBottom: "8px", color: "var(--hotel-text)" }}>{hotel.hotel.address}</p>
-          <p style={{ marginBottom: "8px", color: "gold" }}>
+          <p style={{ margin: "0px", fontSize: "28px", fontWeight: 600, color: "var(--hotel-text)" }}>{hotel.hotel.name}</p>
+          <p style={{ margin: "0px", display: "flex", alignItems: "center", fontSize: "14px", fontWeight: 500, marginBottom: "8px", color: "var(--hotel-caption)" }}>
+            <img
+              src={"/icons/location.svg"}
+              alt="Login"
+              className="w-[14px] h-[14px] mr-[5px]"
+            />
+            {hotel.hotel.address}
+          </p>
+          <p style={{ margin: "0px", color: "gold" }}>
             {Array.from({ length: hotel.hotel.stars }).map((_, i) => (
-              <span key={i} style={{ color: "gold", fontSize: "24px" }}>★</span>
+              <img
+                key={i}
+                src={"/icons/star.svg"}
+                alt="Login"
+                className="w-[18px] h-[22.5px] mr-[5px]"
+              />
             ))}
           </p>
-          <p style={{ color: "var(--hotel-text)", fontSize: "14px", marginTop: "8px" }}>{hotel.hotel.description}</p>
+          <p style={{ whiteSpace: "pre-line", color: "var(--hotel-caption)", fontSize: "14px", marginTop: "8px" }}>{hotel.hotel.description.replace(/<br\s*\/?>/gi, "\n")}</p>
         </div>
-        <div className="text-hotel-text" style={{ width: "100%", maxWidth: "1200px", marginTop: "32px", padding: "0px 24px 0px 24px", display: "flex", flexDirection: "column", gap: "16px" }}>
-          <h2 style={{ fontSize: "20px", fontWeight: 600 }}>Quartos disponíveis</h2>
+        <div className="text-hotel-text" style={{ width: "100%", maxWidth: "1200px", marginTop: "24px", padding: "0px 24px 0px 24px", display: "flex", flexDirection: "column", gap: "16px" }}>
+          <p style={{ margin: "0px", fontSize: "20px", fontWeight: 600 }}>Quartos disponíveis</p>
           {hotel.rooms.map((room: Room, index: number) => (
             <div
               key={index}
@@ -97,26 +109,29 @@ export default function HotelPage() {
                   </p>
                 }
               </div>
-              <div style={{ display: "flex", alignItems: "center", gap: "36px" }}>
-                <p style={{ margin: 0, fontWeight: 500 }}>
-                  {room.price.currency} {room.price.amount}
-                </p>
+              <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between", gap: "36px" }}>
+                <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                  <div style={{ display: "flex", alignItems: "baseline", gap: "4px" }}>
+                    <p style={{ margin: "0px", fontSize: "22px", fontWeight: 600, color: "var(--hotel-primary)" }}>
+                      {room.price.currency.replace("BRL", "R$")} {Math.floor(room.price.amount)}
+                    </p>
+                    <span style={{ fontSize: "14px", color: "var(--hotel-primary)" }}>/ noite</span>
+                  </div>
+                  <p style={{ margin: "0px", fontSize: "14px", color: "var(--hotel-caption)" }}>Pagamento no hotel</p>
+                </div>
                 <button
                   style={{
                     color: "#FFFFFF",
-                    boxShadow: "0 10px 25px rgba(0,0,0,0.15)",
+                    boxShadow: "0 10px 25px rgba(0, 128, 255, 0.25)",
                     border: "none",
                     outline: "none",
-                    width: "8.5vw",
-                    height: "4.5vh",
-                    padding: "0px 24px 0px 24px"
                   }}
-                  className="bg-hotel-primary hover:bg-blue-600 text-[14px] font-semibold rounded-full w-[118px] h-[38px]"
-                onClick={() =>
-                  router.push(
-                    `/hotels/${id}/reserve/${index}?roomName=${encodeURIComponent(room.roomType.name)}`
-                  )
-                }
+                  className="bg-hotel-primary hover:bg-blue-600 text-[14px] font-semibold rounded-full w-[132px] h-[38px]"
+                  onClick={() =>
+                    router.push(
+                      `/hotels/${id}/reserve/${index}?roomName=${encodeURIComponent(room.roomType.name)}`
+                    )
+                  }
                 >
                   Reservar Agora
                 </button>
